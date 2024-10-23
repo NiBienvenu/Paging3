6c8c55fa09ede848011bfe7c27c2804a328e648b
NiBienvenu <nicibienvenu@gmail.com>
2024-10-23 12:29:38 +0200

    Initial commit

---
+++ app/src/main/java/com/bienvenu/paging3/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.bienvenu.paging3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.bienvenu.paging3.presentation.BeerScreen
+import com.bienvenu.paging3.presentation.BeerViewModel
+import com.bienvenu.paging3.ui.theme.ComposePaging3CachingTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePaging3CachingTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val viewModel = hiltViewModel<BeerViewModel>()
+                    val beers = viewModel.beerPagingFlow.collectAsLazyPagingItems()
+                    BeerScreen(beers = beers)
+                }
+            }
+        }
+    }
+}
