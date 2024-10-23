072f0badd5878a77e8601471b1454c18d4ac2760
NiBienvenu <nicibienvenu@gmail.com>
2024-10-23 12:32:53 +0200

    domain

---
+++ app/src/main/java/com/bienvenu/paging3/domain/Beer.kt
@@ -0,0 +1,10 @@
+package com.bienvenu.paging3.domain
+
+data class Beer(
+    val id: Int,
+    val name: String,
+    val tagline: String,
+    val firstBrewed: String,
+    val description: String,
+    val imageUrl: String?
+)
