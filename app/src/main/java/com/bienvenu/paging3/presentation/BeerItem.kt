e358970c71cc91d6ddf2877f1da89ca867b9be6d
NiBienvenu <nicibienvenu@gmail.com>
2024-10-27 06:30:40 +0200

    permission usetrafick

--- app/src/main/java/com/bienvenu/paging3/presentation/BeerItem.kt
+++ app/src/main/java/com/bienvenu/paging3/presentation/BeerItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
